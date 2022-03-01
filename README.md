532916a1d31e6e6b7bc16d8741443520c815b091
Vítor Manfredini <vitormanfredini@gmail.com>
2022-03-01 12:24:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Don't Call It Cookies
+
+## A tutorial on how to add a ~~Cookie~~ Data Collector Consent bar/popup to your website.
+
+This tutorial uses [a lightweight & gdpr compliant cookie consent plugin written in plain javascript](https://github.com/orestbida/cookieconsent).
+
+![Screenshot](https://github.com/vitormanfredini/data-collectors-tutorial/blob/main/screenshot.jpg?raw=true)
+
+Here's how you do it:
+
+1. Download these 2 files: `consent-config.js` and `consent.css`.
+
+2. Upload `consent.css` to your server.
+
+3. In `consent-config.js`:
+
+  * Edit the line `theme_css: 'css/consent.css',` to point to the css file you just uploaded.
+
+  * In this portion: `<a class="cc-link" href="#privacypolicypage">privacy policy</a>`, replace the `#privacypolicypage` with a link to your privacy policy page.
+
+  * In this portion: `<a class="cc-link" href="#yourcontactpage">contact us</a>`, replace the `#yourcontactpage` with a link to your contact page.
+
+4. Upload `consent-config.js` to your server.
+
+5. Include the library and your configuration as follows:
+
+```
+<script src="https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v2.8.0/dist/cookieconsent.js"></script>
+<script src="path-to-your-js-folder/consent-config.js"></script>
+```
+Don't forget to edit the `path-to-your-js-folder` portion with the correct path on your server.
+
+6. Now we tell the library which script tags it should manage and what category they fit into.
+
+a. Edit the attribute `type="text/javascript"` portion to read `type="text/plain"` on every script that collects data, so it doesn't load right away.
+
+b. Add another attribute to specify its category: `data-cookiecategory="ads"`
+Possible values are: `necessary`, `ads` and `analytics`.
+
+Here's an example of what it should look like:
+
+```
+<script type="text/plain" data-cookiecategory="analytics" src="https://www.googletagmanager.com/gtag/js?id=UA-123456789-1"></script>
+<script type="text/plain" data-cookiecategory="analytics">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-123456789-1');
+</script>
+```
+
+7. That's it! You should now see the consent screen appearing on your website.
---
+++ consent.js
@@ -0,0 +1,117 @@
+// obtain plugin
+var cc = initCookieConsent();
+
+// run plugin with your configuration
+cc.run({
+    current_lang: 'en',
+    autoclear_cookies: true,                   // default: false
+    // theme_css: 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v2.8.0/dist/cookieconsent.css',  // 🚨 replace with a valid path
+    theme_css: 'css/consent.css',
+    page_scripts: true,                        // default: false
+
+    // mode: 'opt-in'                          // default: 'opt-in'; value: 'opt-in' or 'opt-out'
+    // delay: 0,                               // default: 0
+    // auto_language: null                     // default: null; could also be 'browser' or 'document'
+    // autorun: true,                          // default: true
+    // force_consent: false,                   // default: false
+    // hide_from_bots: false,                  // default: false
+    // remove_cookie_tables: false             // default: false
+    // cookie_name: 'cc_cookie',               // default: 'cc_cookie'
+    // cookie_expiration: 182,                 // default: 182 (days)
+    // cookie_necessary_only_expiration: 182   // default: disabled
+    // cookie_domain: location.hostname,       // default: current domain
+    // cookie_path: '/',                       // default: root
+    // cookie_same_site: 'Lax',                // default: 'Lax'
+    // use_rfc_cookie: false,                  // default: false
+    // revision: 0,                            // default: 0
+
+    onFirstAction: function(user_preferences, cookie){
+        // callback triggered only once
+    },
+
+    onAccept: function (cookie) {
+        // ...
+    },
+
+    onChange: function (cookie, changed_preferences) {
+        // ...
+    },
+
+    languages: {
+        'en': {
+            consent_modal: {
+                title: 'We use data collectors!',
+                description: 'Hi, this website uses essential data collectors to ensure its proper operation and tracking data collectors to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
+                primary_btn: {
+                    text: 'Accept all',
+                    role: 'accept_all'              // 'accept_selected' or 'accept_all'
+                },
+                secondary_btn: {
+                    text: 'Reject all',
+                    role: 'accept_necessary'        // 'settings' or 'accept_necessary'
+                }
+            },
+            settings_modal: {
+                title: 'Data collection preferences',
+                save_settings_btn: 'Save settings',
+                accept_all_btn: 'Accept all',
+                reject_all_btn: 'Reject all',
+                close_btn_label: 'Close',
+                cookie_table_headers: [
+                    {col1: 'Name'},
+                    {col2: 'Domain'},
+                    {col3: 'Expiration'},
+                    {col4: 'Description'}
+                ],
+                blocks: [
+                    {
+                        title: 'Data collection usage 📢',
+                        description: 'I use data collectors to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to data collectors and other sensitive data, please read the full <a class="cc-link" href="#privacypolicypage">privacy policy</a>.'
+                    }, {
+                        title: 'Strictly necessary data collectors',
+                        description: 'These data collectors are essential for the proper functioning of my website. Without these data collectors, the website would not work properly',
+                        toggle: {
+                            value: 'necessary',
+                            enabled: true,
+                            readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
+                        }
+                    }, {
+                        title: 'Performance and Analytics data collectors',
+                        description: 'These data collectors allow the website to remember the choices you have made in the past',
+                        toggle: {
+                            value: 'analytics',     // your cookie category
+                            enabled: false,
+                            readonly: false
+                        },
+                        /*cookie_table: [             // list of all expected cookies
+                            {
+                                col1: '^_ga',       // match all cookies starting with "_ga"
+                                col2: 'google.com',
+                                col3: '2 years',
+                                col4: 'description ...',
+                                is_regex: true
+                            },
+                            {
+                                col1: '_gid',
+                                col2: 'google.com',
+                                col3: '1 day',
+                                col4: 'description ...',
+                            }
+                        ]*/
+                    }, {
+                        title: 'Advertisement and Targeting data collectors',
+                        description: 'These data collectors collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
+                        toggle: {
+                            value: 'ads',
+                            enabled: false,
+                            readonly: false
+                        }
+                    }, {
+                        title: 'More information',
+                        description: 'For any queries in relation to our policy on data collectors and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
+                    }
+                ]
+            }
+        }
+    }
+});
